179878eec8783f58bce0a351a815738dad879685
Bruno.Rozov <bruno.rozov@voco.ee>
2026-03-18 09:03:23 +0200

    Sain teada milleks pass ja raise vajalikud on :)

--- test.py
+++ test.py
@@ -7,3 +7,32 @@ class Machine:
 
     def scan(self, document):
         raise NotImplementedError("Scan not implemented!")
+    
+class MultiFunctionPrinter(Machine):
+    def print(self, document):
+        print(f"Printing: {document}")
+
+    def fax(self, document):
+        print(f"Faxing: {document}")
+
+    def scan(self, document):
+        print(f"Scanning: {document}")
+
+
+class OldFashionedPrinter(Machine):
+    def print(self, document):
+        print(f"Printing: {document}")
+
+    def fax(self, document):
+        pass # <-- ignoreei funksioon
+
+    def scan(self, document):
+        raise NotImplementedError("Printer cannot scan!") # <-- lööb errori
+
+
+
+printer = OldFashionedPrinter()
+
+printer.print("Hello world")
+printer.fax("Test document") 
+printer.scan("Important document")
